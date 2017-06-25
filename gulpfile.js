eeecf4044bf7f76762990d6a647c44171a0be481
EverGallegos <egallegosdev@gmail.com>
2017-06-25 20:22:33 -0300

    Configurando chart

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 gulp.task('sass', function(){
-	return sass('sass/**/*.sass').pipe(gulp.dest('css'));
+	gulp.src('sass/**/*.sass')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function(){
